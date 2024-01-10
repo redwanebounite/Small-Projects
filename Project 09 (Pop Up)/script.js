7fc3a589602b95fc1b1584c0903f3637a4dd4966
Redwane Bou <155301664+redwanebounite@users.noreply.github.com>
2024-01-10 22:23:16 +0100

    add 'popup project' files

---
+++ Project 09 (Pop Up)/script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+let delet = document.getElementById("delete");
+function show(){
+    popup.classList.add("show")
+}
+
+function hidden(){
+    delet.parentElement.remove();
+}
