24bd7afe6369ad9e5748351bad3be013e4c66751
Redwane Bou <155301664+redwanebounite@users.noreply.github.com>
2024-02-04 13:05:28 +0100

    add project page files

---
+++ Project 17( Product Page)/script.js
@@ -0,0 +1,23 @@
+let img = document.getElementById("img");
+let btn = document.getElementsByClassName("btn");
+btn[0].onclick = function() {
+    img.src = "images/image1.png"
+    for (bt of btn){
+        bt.classList.remove("active")
+    }
+    this.classList.add("active")
+}
+btn[1].onclick = function() {
+    img.src = "images/image2.png"
+    for (bt of btn){
+        bt.classList.remove("active")
+    }
+    this.classList.add("active")
+}
+btn[2].onclick = function() {
+    img.src = "images/image3.png"
+    for (bt of btn){
+        bt.classList.remove("active")
+    }
+    this.classList.add("active")
+}
